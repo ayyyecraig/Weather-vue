cf1cdf9949f1868e8a5f97881f9e40744cc32532
Adelaide <adelaide.craig@outlook.com>
2022-07-04 21:24:04 -0500

    ui

--- vue-weather/src/App.vue
+++ vue-weather/src/App.vue
@@ -4,6 +4,17 @@
      <div class="search-box"> 
        <input type="text" name="" id="" class="search-bar" placeholder="Search...">
      </div>
+
+     <div class="weather-wrap">
+      <div class="location-box">
+        <div class="location">Here</div>
+        <div class="date">July fourth</div>
+      </div>
+      <div class="weather-box">
+        <div class="temp">8</div>
+        <div class="weather">rainy</div>
+      </div>
+     </div>
    </main> 
   </div>
 </template>
@@ -70,5 +81,45 @@ main {
   box-shadow: 0px 0px 16px rgba(0,0,0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: white;
+  font-size:  32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: white;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+.weather-box{
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline;
+  padding: 1px 25px;
+  color: white;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow:3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25)
+}
+
+.weather-box .weather {
+  color: white;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+
 }
 </style>
